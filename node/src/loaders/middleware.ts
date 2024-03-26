0dc66101e34fed3b2480497616c33df4dbd8ce97
Gabriel Phala <gabrielphala@outlook.com>
2024-03-26 10:28:55 +0200

    removed localhost cors restrictions

--- node/src/loaders/middleware.ts
+++ node/src/loaders/middleware.ts
@@ -6,17 +6,17 @@ import cookieParser from "cookie-parser"
 export default (app: Application) => {
     app.use(cookieParser());
 
-    app.use((req, res, next) => {
-        res.set({
-            'Access-Control-Allow-Credentials': true,
-            'Access-Control-Allow-Origin': 'http://localhost:5173',
-            'Access-Control-Allow-Headers': 'Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With, Allow',   
-            'SameSite': 'None',  
-            'Secure': ''    
-        });
+    // app.use((req, res, next) => {
+    //     res.set({
+    //         'Access-Control-Allow-Credentials': true,
+    //         'Access-Control-Allow-Origin': 'http://localhost:5173',
+    //         'Access-Control-Allow-Headers': 'Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With, Allow',   
+    //         'SameSite': 'None',  
+    //         'Secure': ''    
+    //     });
 
-        next();
-    })
+    //     next();
+    // })
 
     // app.use(/^((?!(assets)).)*$/, loadUserInfo)
     app.use(loadUserInfo)
